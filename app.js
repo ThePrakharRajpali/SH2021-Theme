358fc3c2b306f8754464d1b4b2c53c98909bfea6
Prakhar Rajpali <rajpaliprakhar23@gmail.com>
2021-12-12 12:08:02 +0530

    Changed routing

--- app.js
+++ app.js
@@ -12,19 +12,19 @@ app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded());
 
-app.get("/", (req, res) => {
+app.get("/scavengerHunt/", (req, res) => {
   const defaultMessage =
     "What you see is not there and what's there conceals itself beneath a hand, eyes will reveal the object but tolerance must be used to unveil the objective. You might have to rack your brains but will find nothing there, may be the answer is close at hand. Don’t spend too much time looking for something at one place start exploring and you will find it right in front of your eyes.";
   res.render("index", { modalContent: defaultMessage, isWrong: 0 });
 });
 
-app.get("/extra", (req, res) => {
+app.get("/scavengerHunt/extra", (req, res) => {
   const defaultMessage =
     "What you see is not there and what's there conceals itself beneath a hand, eyes will reveal the object but tolerance must be used to unveil the objective. You might have to rack your brains but will find nothing there, may be the answer is close at hand. Don’t spend too much time looking for something at one place start exploring and you will find it right in front of your eyes.";
   res.render("extra");
 });
 
-app.post("/", (req, res) => {
+app.post("/scavengerHunt/", (req, res) => {
   const defaultMessage =
     "What you see is not there and what's there conceals itself beneath a hand, eyes will reveal the object but tolerance must be used to unveil the objective. You might have to rack your brains but will find nothing there, may be the answer is close at hand. Don’t spend too much time looking for something at one place start exploring and you will find it right in front of your eyes.";
   const defaultSuccessMessage =
@@ -38,7 +38,7 @@ app.post("/", (req, res) => {
   }
 });
 
-app.post("/success", (req, res) => {
+app.post("/scavengerHunt/success", (req, res) => {
   const defaultMessage =
     "You made it this far! Pat on your back. Paris, Grenades, Professor. Guess the theme of Scavenger Hunt 2021, and tell us your answer.";
   var answer = req.body.answer;
